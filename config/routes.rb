504d1f555456a6ed3b90e331db84faf72ee029a8
brandtryan <emailme@brandtryan.com>
2024-06-25 16:46:03 -0400

    Finish Section 3.3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "application#hello"
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
